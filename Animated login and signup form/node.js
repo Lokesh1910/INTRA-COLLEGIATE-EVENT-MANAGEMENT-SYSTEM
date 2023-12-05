7c91bc383b21a0631ad35e848aba26b1ef660498
Event <prembalaji1803@gmail.com>
2023-12-06 00:49:05 +0530

    first commit

---
+++ Animated login and signup form/node.js
@@ -0,0 +1,11 @@
+// Synchronous file reading
+const fs = require('fs');
+
+try {
+  const data = fs.readFileSync('example.txt', 'utf8');
+  console.log('File content:', data);
+} catch (err) {
+  console.error('Error reading file:', err);
+}
+
+console.log('Reading file synchronously...');
